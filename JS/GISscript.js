c2b5bdedc7f86bc0870430c6de89012609cd65b9
Jenish Shah <JenishShaleenkumar.Shah@flemingcollege.ca>
2021-12-08 17:01:24 -0500

    js comment

--- JS/GISscript.js
+++ JS/GISscript.js
@@ -1,3 +1,8 @@
+// Owner : Jenish Shaleenkumar Shah //
+// Date : Dec 7 2021 //
+// https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp //
+
+// This function hides the form if it is visible and shows the form if it is hidden, on a single click. //
 function myFunction() {
     var x = document.getElementById("crrform");
     if (x.style.display === "none") {
@@ -6,6 +11,8 @@ function myFunction() {
       x.style.display = "none";
     }
   }
+// https://www.w3schools.com/js/js_popup.asp //
+//This function makes a popup alert appear when the button is pressed, giving the user two choices. //
 function alertfunc() {
     var txt;
     if (confirm("Please Confirm!")) {
@@ -15,3 +22,4 @@ function alertfunc() {
     }
     document.getElementById("apply").innerHTML = txt;
   }
+// End Ownership : Jenish Shaleenkumar Shah //
